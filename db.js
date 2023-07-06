fb41ce12dc45cc5704aab239210edfc407a602c1
Maxwell Hanson <m47hanson47@gmail.com>
2023-07-06 16:43:55 -0400

    fixed ECONNREFUSED by changing to 127.0.0.1:27017

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require("mongodb");
 
-let dbConnection
+let dbConnection;
 
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect('mongodb://localhost:27017/Bookstore')
-      .then(client => {
-        dbConnection = client.db()
-        return cb()
+    MongoClient.connect("mongodb://127.0.0.1:27017/Bookstore")
+      .then((client) => {
+        dbConnection = client.db();
+        return cb();
       })
-      .catch(err => {
-        console.log(err)
-        return cb(err)
-      })
+      .catch((err) => {
+        console.log(err);
+        return cb(err);
+      });
   },
-  getDb: () => dbConnection
-}
+  getDb: () => dbConnection,
+};
